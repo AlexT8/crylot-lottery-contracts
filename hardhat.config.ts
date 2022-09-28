18cefb662c7a2e1cea5c50fc313ffef76acc42cc
Alex Taboada <alexandrotaboada@gmail.com>
2022-09-28 13:30:58 -0300

    deploy to DevNet

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,14 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
+  networks: {
+    development: {
+      url: "http://127.0.0.1:7545",     // Localhost (default: none)
+      // @ts-ignore
+      port: 7545,            // Standard Ethereum port (default: none)
+      network_id: "*"        // Any network (default: none)
+     }
+  }
 };
 
 export default config;
