c42ac11d40a455f263b558dabd19665449619fba
Alex Taboada <alexandrotaboada@gmail.com>
2022-09-28 16:48:05 -0300

    deployment to Goerli

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,9 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+import dotenv from 'dotenv'
+dotenv.config();
+
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
   networks: {
@@ -9,7 +12,11 @@ const config: HardhatUserConfig = {
       // @ts-ignore
       port: 7545,            // Standard Ethereum port (default: none)
       network_id: "*"        // Any network (default: none)
-     }
+    },
+    goerli: {
+    url: `https://goerli.infura.io/v3/${process.env.INFURA_APIKEY}`, //Infura url with projectId
+      accounts: [process.env.SIGNER_PRIVATE_KEY || ''] // add the account that will deploy the contract (private key)
+    }
   }
 };
 
